fc294964de1541d1fe52d4ec6711a75cacb764ef
Matti Risteli <matti.risteli@iki.fi>
2019-10-22 21:17:34 +0300

    4.4 4.5 some proper helper functions and tests done

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,27 @@
 const dummy = (blogs) => {
-  return 1
+  return blogs.length + 1 - blogs.length
+}
+
+const totalLikes = blogs => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+
+  return blogs.reduce(reducer, 0)
+}
+
+const favoriteBlog = blogs => {
+  const reducer = (currentFavorite, item) => {
+    return currentFavorite.likes < item.likes
+      ? item
+      : currentFavorite
+  }
+
+  return blogs.length === 0
+    ? {}
+    : blogs.reduce(reducer, blogs[0])
 }
 
 module.exports = {
-  dummy
+  dummy, totalLikes, favoriteBlog
 }
